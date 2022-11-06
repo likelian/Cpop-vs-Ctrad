15ec2d0c6bdf5b4e5cc459e4a82f1df54074116f
likelian <likelian@me.com>
2022-11-06 17:34:36 -0500

    interveral distribution plot

--- src/count_interval.py
+++ src/count_interval.py
@@ -4,6 +4,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
+
+
+
 def collect_interval(stream, interval_list):
     """
     input: music21 stream object
@@ -29,12 +32,12 @@ def collect_interval(stream, interval_list):
 
 
 def list_to_plot(interval_list, output_path, dataset_name):
-    
+
     interval_list_str = []
     for interval in interval_list:
         interval_list_str.append(interval.name)
 
-    interval_label_list = ["P1", "m2", "M2", "m3", "M3", "P4", "A4/d5", "P5", "m6", "M6", "m7", "M7", "P8", "m9", "M9", "m10", "M10"]
+    interval_label_list = ["m2", "M2", "m3", "M3", "P4", "A4/d5", "P5", "m6", "M6", "m7", "M7", "P8"]
 
     interval_dict = {}
     for label in interval_label_list:
@@ -49,6 +52,8 @@ def list_to_plot(interval_list, output_path, dataset_name):
 
     fig = sns.barplot(data=interval_df)
     fig.set(xlabel='intervals', ylabel='counts')
+    fig.set(title='Interval Distribution of ' + dataset_name)
     fig = fig.get_figure()
     #plt.show()
     fig.savefig(output_path + dataset_name + ".png")
+    plt.close()
--- src/krn_to_21.py
+++ src/krn_to_21.py
@@ -5,25 +5,45 @@ from count_interval import *
 
 
 
+def plot_dataset_interval(dataset_name, dataset_path, output_path):
 
-dataset_name = "CoCoPops"
+    interval_list = []
+
+    counter = 0
+
+    for root, dirs, files in os.walk(dataset_path + dataset_name):
+        for file in files:
+            if ".krn" in file or ".mid" in file:
+                print("file", file)
+                path = root + "/" + file
+                stream = converter.parse(path)
+                collect_interval(stream, interval_list)
+
+    list_to_plot(interval_list, output_path, dataset_name)
+
+    interval_list = []
+
+
+
+
+
 dataset_path = "../data/"
 output_path = "../results/"
 
-interval_list = []
-
-for root, dirs, files in os.walk(dataset_path + dataset_name):
-    for file in files:
-        if ".krn" in file or ".mid" in file:
-            print("file", file)
-            path = root + "/" + file
-            stream = converter.parse(path)
-            collect_interval(stream, interval_list)
-
-            #remove
-            break
 
 
-list_to_plot(interval_list, output_path, dataset_name)
+
+dataset_name = "CoCoPops"
+plot_dataset_interval(dataset_name, dataset_path, output_path)
 
 
+
+
+dataset_name = "han"
+plot_dataset_interval(dataset_name, dataset_path, output_path)
+
+
+
+
+dataset_name = "POP909"
+plot_dataset_interval(dataset_name, dataset_path, output_path)
